753930222490b00f6646b072449e4c3576388ce0
aarroz <aarroz@users.noreply.github.com>
2018-06-24 23:11:26 -0700

    changed app id

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/home/gsc/steamcmd.sh +login anonymous +force_install_dir /home/gsc/starbound +app_update 211820 validate +quit
+/home/gsc/steamcmd.sh +login anonymous +force_install_dir /home/gsc/starbound +app_update 533830 validate +quit
